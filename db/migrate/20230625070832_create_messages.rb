c94b87d5bdd32b5b48dc6eef7d805ce5955c6c09
tsubi-kaiju <tsugu.no.1@gmail.com>
2023-06-25 18:25:33 +0900

    メッセージの表示

--- db/migrate/20230625070832_create_messages.rb
+++ db/migrate/20230625070832_create_messages.rb
@@ -4,6 +4,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
       t.string  :content
       t.references :room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.timestamps
     end
   end
 end
