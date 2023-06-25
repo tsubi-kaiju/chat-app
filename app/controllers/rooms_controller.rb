882908a4ee592b0d2cafc208c871f7baa39ac905
tsubi-kaiju <tsugu.no.1@gmail.com>
2023-06-25 14:47:44 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,21 @@
 class RoomsController < ApplicationController
+  
   def new
     @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_prarams)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_prarams
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
